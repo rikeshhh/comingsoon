dacccea8187e76db95a18cfc9e724c50ba65b28b
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-11-11 23:14:17 +0545

    fix:mobile view

--- src/section/comingSoon.tsx
+++ src/section/comingSoon.tsx
@@ -1,4 +1,4 @@
 export default function ComingSoon(){
-    return <section className="h-screen font-bagel bg-coming-soon bg-cover w-full ">
+    return <section className="min-h-screen font-bagel bg-coming-soon bg-cover w-full bg-no-repeat max-sm:bg-cover max-sm:bg-center">
     </section>
 }
