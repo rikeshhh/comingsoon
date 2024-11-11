f82910817dd8807e3ac780ed960eb5d1b30a1aa9
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-11-11 22:09:43 +0545

    feat:coming soon

--- src/section/comingSoon.tsx
+++ src/section/comingSoon.tsx
@@ -1,5 +1,4 @@
 export default function ComingSoon(){
-    return <section className="h-screen flex justify-center items-center">
-        <h1>Coming Soon</h1>
+    return <section className="h-screen font-bagel bg-coming-soon bg-cover w-full ">
     </section>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,19 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/section/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage:{
+        'coming-soon' : "url('/ComingSoon.png')"
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        bagel: ['var(--font-bagel-fat-one)'],
       },
     },
   },
