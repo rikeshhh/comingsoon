eaf2485191cbf1e9502413abcb1968eeb9ccf618
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2025-01-02 23:30:30 +0545

    fix:overlap issue and aniamted cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
 
+import AnimatedCursor from "react-animated-cursor";
+
 import ComingSoon from "@/section/comingSoon";
 
 export default function Home() {
   return (
-  <ComingSoon />
+    <>
+    <ComingSoon />
+    <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+        outerAlpha={0}
+        innerStyle={{
+          backgroundColor: '#7E76FF',
+        }}
+        outerStyle={{
+          border: '3px solid #7E76FF',
+        }}
+      />
+    </>
   );
 }
--- src/section/comingSoon.tsx
+++ src/section/comingSoon.tsx
@@ -1,7 +1,7 @@
 export default function ComingSoon() {
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center font-bagel bg-[#4f46e5] text-white  w-full">
-      <div className="flex flex-col justify-center gap-6 items-center container mx-auto text-center -mt-[149px] md:mt-0">
+    <section className="h-full md:h-screen flex flex-col justify-center items-center font-bagel bg-[#4f46e5] text-white  w-full">
+      <div className="flex flex-col justify-center gap-6 items-center container mx-auto text-center">
         <video
           autoPlay
           loop
@@ -11,7 +11,7 @@ export default function ComingSoon() {
           <source src="/sweven.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>{" "}
-       <div className="flex flex-col justify-center items-center -mt-12 gap-2">
+       <div className="flex flex-col justify-center items-center  gap-2">
        <h1 className="text-3xl md:text-6xl font-black ">Website Coming Soon...</h1>
        <p className=" tracking-wider max-md:text-xs font-montserrat">Still figuring things out, Isn’t that life? 😄 </p>
        </div>
