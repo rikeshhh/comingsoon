56d2d153ccc8bd6a61e6ed8d396eed48d1e4ef2b
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2025-01-02 22:29:34 +0545

    fix:spelling error

--- src/section/comingSoon.tsx
+++ src/section/comingSoon.tsx
@@ -1,7 +1,7 @@
 export default function ComingSoon() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center font-bagel bg-[#4f46e5] text-white  w-full">
-      <div className="flex flex-col justify-center gap-4 items-center container mx-auto">
+      <div className="flex flex-col justify-center gap-6 items-center container mx-auto text-center -mt-[149px] md:mt-0">
         <video
           autoPlay
           loop
@@ -11,8 +11,10 @@ export default function ComingSoon() {
           <source src="/sweven.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>{" "}
-        <h1 className="text-4xl md:text-6xl font-black">Coming Soon...</h1>
-        <p className=" tracking-wider md:tracking-[8px] font-montserrat">Under Construction</p>
+       <div className="flex flex-col justify-center items-center -mt-12 gap-2">
+       <h1 className="text-3xl md:text-6xl font-black ">Website Coming Soon...</h1>
+       <p className=" tracking-wider max-md:text-xs font-montserrat">Still figuring things out, Isn’t that life? 😄 </p>
+       </div>
       </div>
     </section>
   );
